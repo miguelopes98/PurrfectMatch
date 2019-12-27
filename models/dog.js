9cdf6220470ca3b3d367ef7830c282ef030ae4af
Miguel Lopes <miguelnflopes98@gmail.com>
2019-12-27 16:02:42 +0000

    adding likes to dog Schema

--- models/dog.js
+++ models/dog.js
@@ -21,6 +21,12 @@ var dogSchema = new mongoose.Schema({
 			type: mongoose.Schema.Types.ObjectId,
 			ref: "Comment"
 		}
+	],
+	likes: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User"
+		}
 	]
 	//for some reason mongoDB collections only auto create when we have at least one parameter unique:true in the schema, in this case, we don't want this anywhere, and mongoDB also creates a collections (if non existing) when we create a dog, so it's fine
 });
